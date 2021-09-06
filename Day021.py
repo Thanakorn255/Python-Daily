28854519c387bfd29a3fa07e592b883e608fa044
I AM TON <69200919+Thanakorn255@users.noreply.github.com>
2021-09-06 10:30:33 +0700

    factorial

--- Day021.py
+++ Day021.py
@@ -1,1 +1,11 @@
-num = int(input())
+def factorial(x):
+    if x ==1 or x==0:
+        return 1
+    else:
+        return x*factorial(x-1)
+
+num = [int(x) for x in input().split()]
+list1 = list()
+for i in num:
+    list1.append(factorial(i))
+print(list1)
